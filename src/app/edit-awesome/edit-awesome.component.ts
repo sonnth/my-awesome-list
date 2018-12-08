a6e29e8c2f89479ce4701e8310c93f8f06eaf819
sonnth <sonnth.humg@gmail.com>
2018-12-08 09:51:52 +0700

    Add edit awesome method

---
+++ src/app/edit-awesome/edit-awesome.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-awesome',
+  templateUrl: './edit-awesome.component.html',
+  styleUrls: ['./edit-awesome.component.css']
+})
+export class EditAwesomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
